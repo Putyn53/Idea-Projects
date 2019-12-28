e194adc2c5c7a2740e4eb2e8419f3ab39bb4cffa
ThePutek <kacput@gmail.com>
2019-12-28 18:18:17 +0100

    28/12

--- Pharm/Farmakologia_create.sql
+++ Pharm/Farmakologia_create.sql
@@ -10,7 +10,7 @@ CREATE TABLE Lekarz (
     Idlekarz int  NOT NULL AUTO_INCREMENT,
     Imie varchar(50)  NOT NULL,
     Nazwisko varchar(50)  NOT NULL,
-    Nr_ewidencyjny varchar(12)  NOT NULL,
+    Nr_ewidencyjny varchar(6)  NOT NULL,
     CONSTRAINT Lekarz_pk PRIMARY KEY (Idlekarz)
 );
 
@@ -55,8 +55,6 @@ CREATE TABLE Recepta (
     Nr_recepta varchar(20)  NOT NULL,
     Idlekarz int  NOT NULL,
     Idpacjent int  NOT NULL,
-    Data_wystawienia date  NOT NULL,
-    Data_wygasniecia date  NULL,
     CONSTRAINT Recepta_pk PRIMARY KEY (Idrecepta)
 );
 
@@ -124,6 +122,112 @@ ALTER TABLE Recepta ADD CONSTRAINT Recepta_Pacjent
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
--- Creation of
+-- Inserting Values
+INSERT INTO Pacjent (Imie, Nazwisko, PESEL, Adres)
+VALUES
+('Kazimierz', 'Kowalski', '12345678901', 'Zlota 59, 00-120 Warszawa'),
+('Luke', 'Skywalker', '23456789012', 'Pustynia 35, 35-000 Jakku'),
+('Han', 'Solo', '34567890123', 'Hangar 35, 99-900 Hoth'),
+('Radanat', 'Walko', '45678901234', 'Mulacka 1, 00-000 Nigdzie'),
+('Garosh', 'Hellscream', '56789012345', 'Dolina Honoru, 03-130 Orgrimmar'),
+('Renfri', 'Blacksun', '67890123456', 'Rynek 21, 59-138 Blaviken');
+
+INSERT INTO Lekarz (Imie, Nazwisko, Nr_ewidencyjny)
+VALUES
+('Tyrande', 'Whisperwind', '000001'),
+('Malfurion', 'Stormrage', '000012'),
+('Anduin', 'Wrynn', '000123'),
+('Angela', 'Zieger', '001234'),
+('Doktor', 'House', '012345');
+
+INSERT INTO Lekarstwo (Nazwa_lekarstwo)
+VALUES
+('Pentanol'),
+('Skorokrzew'),
+('Widrygol'),
+('Golden Apple'),
+('NukaCola'),
+('Immanatol'),
+('Sektaris'),
+('Mewa'),
+('Grom');
+
+INSERT INTO Producent (Nazwa_producent, Adres)
+VALUES
+('Umbrella', 'End of the world 21'),
+('Bialogon', 'Zakret 21, Temeria'),
+('GnomHeal', 'Mountain 31, Gnomeregan');
+
+INSERT INTO Postac_produkt (Nazwa_postac)
+VALUES
+('Żel'),
+('Proszek'),
+('Tabletka'),
+('Roslina'),
+('Mikstura');
+
+INSERT INTO Produkt (Nazwa_produkt, Idproducent, Idlekarstwo, Idpostac)
+VALUES
+('Ibuprom', 1, 1, 3),
+('Henol', 1, 2, 2),
+('Sepson', 1, 3, 5),
+('Strepsils', 4, 1, 3),
+('Xylogel', 2, 5, 1),
+('Xylothan', 2, 6, 5),
+('Svenologel', 3, 7, 4),
+('Tranix', 2, 8, 4),
+('Theraflu', 3, 1, 2),
+('Health Potion', 3, 2, 5),
+('Pikaxium', 3, 3, 4),
+('Seiogen', 2, 9, 2),
+('Emeraldian', 1, 9, 1),
+('Nurofen', 2, 6, 2),
+('Etopiryna', 2, 5, 1),
+('Raviscron', 3, 2, 2),
+('Mikron Night', 1, 2, 4),
+('Ibuprom Noc', 1, 4, 5),
+('K2C', 3, 4, 1),
+('ITN', 1, 7, 2),
+('Semiraklum', 2, 6, 3),
+('Easilium', 2, 8, 3),
+('Hexogrom', 2, 1, 4),
+('Mewian', 1, 2, 5);
+
+INSERT INTO Recepta (Nr_recepta, Idlekarz, Idpacjent)
+VALUES
+('A0457', 1, 2),
+('A0123', 1, 1),
+('A0345', 2, 3),
+('A0453', 3, 1),
+('A0457', 4, 6),
+('A0987', 5, 5),
+('A0412', 5, 4),
+('A0897', 5, 1),
+('A2345', 4, 5);
+
+INSERT INTO Recepta_Lekarstwo(Idrecepta, Idlekarstwo)
+VALUES
+(1,2),
+(2,3),
+(4,5),
+(3,2),
+(4,4),
+(1,3),
+(1,1),
+(2,3),
+(4,4),
+(6,1),
+(4,3),
+(6,1),
+(2,3),
+(1,4),
+(3,5),
+(1,6),
+(5,1),
+(4,2),
+(5,3),
+(2,4),
+(1,5);
+
 -- End of file.
 
